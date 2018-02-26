d42118e601e252fd7d7dda3f597370617f0c1339
王志盼 <1204473090@qq.com>
2018-02-26 21:44:39 +0800

    修改bug

--- RetryNetwork/NetworkManager/ZYRequestManager.m
+++ RetryNetwork/NetworkManager/ZYRequestManager.m
@@ -22,8 +22,6 @@
 
 @property (nonatomic, strong) dispatch_semaphore_t semaphore;
 
-//这个串行队列用来控制任务有序的执行
-@property (nonatomic, strong) dispatch_queue_t taskQueue;
 
 //添加、删除队列，维护添加与删除request在同一个线程
 @property (nonatomic, strong) dispatch_queue_t addDelQueue;
@@ -40,7 +38,7 @@ static id _instance = nil;
 
 
 //最大并发数
-static const int _maxCurrentNum = 4;
+static const int _maxCurrentNum = 3;
 
 @implementation ZYRequestManager
 + (instancetype)sharedInstance
@@ -79,80 +77,76 @@ static const int _maxCurrentNum = 4;
     }
     
     [self queueAddRequest:request successBlock:successBlock failureBlock:failedBlock];
-    [self dealRequestQueue];
+    
 }
 
 - (void)dealRequestQueue
 {
-    if (self.isRetaining) return;
-    self.isRetaining = true;
+//    if (self.isRetaining) return;
+//    self.isRetaining = true;
     
     //在子线程轮询，以免阻塞主线程
     //让请求按队列先后顺序发送
-    dispatch_async(self.taskQueue, ^{
+    
         
-        while (self.requestQueue.count > 0)
-        {
-            ZYRequest *request = self.requestQueue.firstObject;
-            SuccessBlock successBlock = self.successQueue.firstObject;
-            FailedBlock failedBlock = self.failureQueue.firstObject;
+    while (self.requestQueue.count > 0)
+    {
+        ZYRequest *request = self.requestQueue.firstObject;
+        SuccessBlock successBlock = self.successQueue.firstObject;
+        FailedBlock failedBlock = self.failureQueue.firstObject;
+        [self queueRemoveFirstObj];
+        NSLog(@"----------------[%d]", request.requestId);
+        dispatch_semaphore_wait(self.semaphore, DISPATCH_TIME_FOREVER);
+        //利用AFN发送请求
+        [[YQDHttpClinetCore sharedClient] requestWithPath:request.urlStr method:request.method parameters:request.params prepareExecute:nil success:^(NSURLSessionDataTask *task, id responseObject) {
+            dispatch_semaphore_signal(self.semaphore);
             
-            
-            dispatch_sync(self.addDelQueue, ^{
-                [self queueRemoveFirstObj];
-            });
-            dispatch_semaphore_wait(self.semaphore, DISPATCH_TIME_FOREVER);
-            //利用AFN发送请求
-            [[YQDHttpClinetCore sharedClient] requestWithPath:request.urlStr method:request.method parameters:request.params prepareExecute:nil success:^(NSURLSessionDataTask *task, id responseObject) {
-                dispatch_semaphore_signal(self.semaphore);
-                
 //                NSLog(@"++++++++%d", request.requestId);
-                //在这里可以根据状态码处理相应信息、序列化数据、是否需要缓存等
-                if (request.cacheKey)
+            //在这里可以根据状态码处理相应信息、序列化数据、是否需要缓存等
+            if (request.cacheKey)
+            {
+                NSError *error = nil;
+                NSData *data = [NSJSONSerialization dataWithJSONObject:responseObject options:NSJSONWritingPrettyPrinted error:&error];
+                
+                if (!error)
                 {
-                    NSError *error = nil;
-                    NSData *data = [NSJSONSerialization dataWithJSONObject:responseObject options:NSJSONWritingPrettyPrinted error:&error];
-                    
-                    if (!error)
-                    {
-                        [[ZYRequestCache sharedInstance] saveData:data ForKey:request.cacheKey];
-                    }
+                    [[ZYRequestCache sharedInstance] saveData:data ForKey:request.cacheKey];
                 }
-                
-                //在成功的时候移除realm数据库中的缓存
-                if (request.reliability == ZYRequestReliabilityStoreToDB)
-                {
-                    [[ZYRequestCache sharedInstance] deleteRequestFromRealmWithRequestId:request.requestId];
-                }
-                
-                successBlock(responseObject);
-                
-            } failure:^(NSURLSessionDataTask *task, NSError *error) {
-                
-                dispatch_semaphore_signal(self.semaphore);
-                
-                //请求失败之后，根据约定的错误码判断是否需要再次请求
-                //这里，-1001是AFN的超时error
-                if (error.code == -1001 &&request.retryCount > 0)
-                {
-                    [request reduceRetryCount];
-                    [self queueAddRequest:request successBlock:successBlock failureBlock:failedBlock];
-                    [self dealRequestQueue];
-                }
-                else  //处理错误信息
-                {
-                    failedBlock(error);
-                }
-            }];
-        }
+            }
+            
+            //在成功的时候移除realm数据库中的缓存
+            if (request.reliability == ZYRequestReliabilityStoreToDB)
+            {
+                [[ZYRequestCache sharedInstance] deleteRequestFromRealmWithRequestId:request.requestId];
+            }
+            
+            successBlock(responseObject);
+            
+        } failure:^(NSURLSessionDataTask *task, NSError *error) {
+            
+            dispatch_semaphore_signal(self.semaphore);
+            
+            //请求失败之后，根据约定的错误码判断是否需要再次请求
+            //这里，-1001是AFN的超时error
+            if (error.code == -1001 &&request.retryCount > 0)
+            {
+                [request reduceRetryCount];
+                [self queueAddRequest:request successBlock:successBlock failureBlock:failedBlock];
+                [self dealRequestQueue];
+            }
+            else  //处理错误信息
+            {
+                failedBlock(error);
+            }
+        }];
+    }
         
-        if (self.requestQueue.count == 0)
-        {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                self.isRetaining = false;
-            });
-        }
-    });
+//        if (self.requestQueue.count == 0)
+//        {
+//            dispatch_async(dispatch_get_main_queue(), ^{
+//                self.isRetaining = false;
+//            });
+//        }
     
 }
 
@@ -165,7 +159,7 @@ static const int _maxCurrentNum = 4;
         return;
     }
     
-    dispatch_sync(self.addDelQueue, ^{
+    dispatch_async(self.addDelQueue, ^{
         
         if ([self.requestQueue containsObject:request]) return;
         
@@ -185,6 +179,8 @@ static const int _maxCurrentNum = 4;
             tmpBlock = [^(id obj){} copy];
         }
         [self.failureQueue addObject:tmpBlock];
+        
+        [self dealRequestQueue];
     });
 }
 
@@ -251,14 +247,6 @@ static const int _maxCurrentNum = 4;
     return _failureQueue;
 }
 
-- (dispatch_queue_t)taskQueue
-{
-    if (!_taskQueue)
-    {
-        _taskQueue = dispatch_queue_create("com.xxxx.www", DISPATCH_QUEUE_SERIAL);
-    }
-    return _taskQueue;
-}
 
 - (dispatch_queue_t)addDelQueue
 {
